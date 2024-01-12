841fffc7b0fdd056ebfced2956d9d157a7e2c514
mariozenmedina <mazen.mario@gmail.com>
2024-01-12 02:37:41 -0300

    backup

--- header.php
+++ header.php
@@ -26,5 +26,14 @@
 	<link rel="shortcut icon" href="./img/favicon.ico" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 	<link rel="stylesheet" href="./css/less.css" type="text/css" media="all">
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-H84PNYMGVR"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-H84PNYMGVR');
+	</script>
 </head>
 <body>
